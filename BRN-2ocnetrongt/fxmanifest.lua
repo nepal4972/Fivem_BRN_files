512182885e082907f948b9d7a137aa2a58518e7d
Saugat Nepal <83812124+nepal4972@users.noreply.github.com>
2022-06-18 19:15:55 +0545

    Add files via upload

---
+++ BRN-2ocnetrongt/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+author 'ALEN TL'
+version '1.0'
+game 'gta5'
+
+files {
+    'data/*.meta'
+}
+
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'DLCTEXT_FILE' 'data/dlctext.meta'
