4dc186bdf91f1c89ac6cc96975f617ad3a7ba0f3
Saugat Nepal <83812124+nepal4972@users.noreply.github.com>
2022-06-18 19:48:45 +0545

    Add files via upload

---
+++ BRN-widebodyled/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'bodacious'
+
+game 'gta5'
+
+lua54 'yes'
+
+files {
+
+    'handling.meta',
+	'vehiclelayouts.meta',
+    'vehicles.meta',
+    'carvariations.meta',
+    'dlctext.meta',
+
+    
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'DLCTEXT_FILE' 'dlctext.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
